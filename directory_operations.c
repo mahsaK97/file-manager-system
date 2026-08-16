c0054fbb92c739aadadc69f12124c6371462a668
Suha <mahsa.keshtpur.1005@gmail.com>
2026-08-16 20:35:35 +0430

    Update directory functions and fix bugs

--- directory_operations.c
+++ directory_operations.c
@@ -35,11 +35,29 @@ void create_folder(FileManager *fm)
 
     fgets(fm->buffer , 1024 , stdin);
     char answer = fm->buffer[0];
+    if(answer =='\0')
+    {
+        printf("ANSWER CAN'T BE EMPTY.\n");
+        return;
+    }
     if(answer == 'y' || answer == 'Y')
     {
         printf("enter name of folder:\n");
         fgets(name_of_folder , sizeof(name_of_folder) , stdin);
         name_of_folder[strcspn(name_of_folder , "\n")] = '\0';
+        if(name_of_folder[0] =='\0')
+        {
+            printf("FOLDER NAME CAN'T BE EMPTY.\n");
+            return;
+        }
+
+        DIR *dir = opendir(name_of_folder);
+        if(dir != NULL)
+        {
+            printf("THERE IS A FOLDER WITH THIS NAME ALREDY.\n");
+            closedir(dir);
+            return;
+        }
         if(MKDIR(name_of_folder) == 0)
         {
              printf("folder created successfully.\n");
@@ -76,8 +94,6 @@ void delete_folder(FileManager *fm)
     printf("do you want to delete a folder?[y/n]\n");
     fgets(fm -> buffer , 1024 , stdin);
     char answer = fm->buffer[0];
-
-
     if(answer == 'N' || answer =='n')
     {
         printf("folder is not deleted.\n");
@@ -92,6 +108,14 @@ void delete_folder(FileManager *fm)
         if(folder_name[0] == '\0')
         {
             printf("folder name cannot be empty.\n");
+            return;
+        }
+
+        DIR *dir = opendir(folder_name);
+        if(dir == NULL)
+        {
+            printf("THERE IS NO FOLDER WITH THIS NAME.\n");
+            closedir(dir);
             return;
         }
         else if(RMDIR(folder_name) == 0)
@@ -142,6 +166,11 @@ void file_list_in_folder(FileManager *fm)
         printf("enter folder name: ");
         fgets(folder_name , sizeof(folder_name) , stdin);
         folder_name[strcspn(folder_name,"\n")] = '\0';
+        if(folder_name[0] == '\0')
+        {
+            printf("FOLDER NAME CAN'T BE EMPTY.\n");
+            return;
+        }
 
         dir = opendir(folder_name);
 
